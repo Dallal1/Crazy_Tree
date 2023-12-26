2cfbd99c28d24cf5903dc1898f1c1b6c653fcd8b
Dallal1 <abw090571@gmail.com>
2023-12-26 22:48:29 +0100

    .

---
+++ crazy_tree/main.cpp
@@ -0,0 +1,77 @@
+#include "raylib.h"
+#include <iostream>
+
+//using namespace Image;
+
+Texture2D tree;
+
+//Texture2D
+class Imag
+{
+public:
+	int posX = 0;
+	int posY = 75;
+
+	void Draw()
+	{
+		DrawTexture(tree, posX, posY, WHITE);
+	}
+};
+
+//Ball
+class Ball
+{
+public:
+
+	float centerX = 390;
+	float centerY = 315;
+	float radius = 10;
+	int speedX = 1;
+	int speedY = 1;
+
+	void Draw()
+	{
+		DrawCircle(centerX, centerY, radius, BLUE);
+	}
+
+	void Update()
+	{
+		centerX = centerX + speedX;
+		centerY = centerY - speedY;
+	}
+};
+
+
+Imag imag;
+Ball ball;
+int main()
+{
+	//Screen Size
+	const int Screen_width = 800;
+	const int Screen_Higth = 450;
+	InitWindow(Screen_width, Screen_Higth, "Crazy Tree");
+
+	tree = LoadTexture("Graphics/tree.png");
+
+	 //FPS
+	 SetTargetFPS(144);
+
+	//Screen Loop
+	while (WindowShouldClose() == false)
+	{
+		BeginDrawing();
+			ClearBackground(BLACK);
+
+			//Update
+			ball.Update();
+			//Draw
+			imag.Draw();
+			ball.Draw();
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+
+	return (0);
+}
